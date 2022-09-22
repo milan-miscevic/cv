269f5c425a198cbc47053a1bc8984e65c154e76d
Milan Miščević <milan-miscevic@users.noreply.github.com>
2022-09-22 22:58:22 +0200

    More precise types in Profile

--- src/Profile/Position.php
+++ src/Profile/Position.php
@@ -10,7 +10,7 @@ class Position
 {
     public function __construct(
         public string $role,
-        public string $company,
+        public ?string $company,
         public DateTimeInterface $startDate,
         public ?DateTimeInterface $endDate,
         public ?string $aboutCompany,
